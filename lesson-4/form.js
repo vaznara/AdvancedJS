cf9404b990a56560fad821de524f922b71c8078b
Vaznara <vaznara@gmail.com>
2019-06-12 21:02:23 +0400

    Переделал на ООП, добавил ошибки.

--- lesson-4/form.js
+++ lesson-4/form.js
@@ -1,29 +1,51 @@
-    document.querySelector('#submit').addEventListener('click', () => {
-
-    const regexpName = new RegExp('^[a-z]+$', 'gi');
-    const regexpPhone = new RegExp('^\\+\\d\\(\\d{3}\\)\\d{3}-\\d{4}$', 'gi');
-    const regexpEmail = new RegExp('^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$', 'gi');
-
-    let nameField = document.querySelector('#name');
-    let phoneField = document.querySelector('#phone');
-    let emailField = document.querySelector('#email');
-
-    if (!regexpName.test(nameField.value)) {
-        nameField.classList.add('red');
-    } else {
-        nameField.classList.remove('red');
+class inputValidator {
+    constructor(form) {
+        this.form = form;
+        this.regExp = {
+            name: /^[a-z]+$/gi,
+            phone: /^\+\d\(\d{3}\)\d{3}-\d{4}$/gi,
+            email: /^([a-z0-9_.-]+)@([a-z0-9_.-]+).([a-z.]{2,6})$/gi
+        };
+        this.errorText = {
+            name: `Имя должно содержать только буквы`,
+            phone: `Телефон должен иметь след. формат +7(000)000-0000`,
+            email: `email должен иметь след. формат test@test.com`
+        }
+        this.init();
     }
 
-    if (!regexpPhone.test(phoneField.value)) {
-        phoneField.classList.add('red');
-    } else {
-        phoneField.classList.remove('red');
+    _validateField() {
+
+        let isValidated = true;
+        for (let data in this.regExp) {
+            let validateData = this.form.querySelector(`[data-${data}]`)
+            if (!this.regExp[data].test(validateData.value)) {
+                validateData.classList.add('red');
+                isValidated = false;
+                if(!validateData.value) {
+                    this.form.querySelector(`.error_text[data-${data}]`).innerHTML = 'Поле должно быть заполнено.';
+                } else {
+                    this.form.querySelector(`.error_text[data-${data}]`).innerHTML =`${this.errorText[data]}`;
+                }
+
+            } else {
+                validateData.classList.remove('red');
+                this.form.querySelector(`.error_text[data-${data}]`).innerHTML =``;
+            }
+        }
+
+        return isValidated;
     }
 
-    if (!regexpEmail.test(emailField.value)) {
-        emailField.classList.add('red');
-    } else {
-        emailField.classList.remove('red');
+    init() {
+        if(this._validateField()) {
+            this.form.submit();
+        }
     }
+}
 
+
+document.querySelector('form').addEventListener('submit', () => {
+    event.preventDefault();
+    let validate = new inputValidator(event.currentTarget);
 })
